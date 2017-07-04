763d9937c1d4a103e73887920bf181a947df4811
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-04 11:10:20 +0300

    commit

--- src/SummerHolydays.java
+++ src/SummerHolydays.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by admin on 04.07.2017.
  */
 public class SummerHolydays {
+    public static void main(String[] args) {
+        System.out.println("Як я проведу літо? ... буду вчитись на GoIT!");
+    }
 }
